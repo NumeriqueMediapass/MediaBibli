e4705f9c6710798e015f4c2b713ec04ad86d431b
NumeriqueMediapass <97108971+NumeriqueMediapass@users.noreply.github.com>
2022-12-26 16:41:18 +0100

    Update settings.py
    Changement de la SECRET_KEY pour Github

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from config import SECRET_KEY
+#from config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY
+#SECRET_KEY
+SECRET_KEY = ck9l@x_05%4d(k2%1a28bzdzn4=wjokm438z$8-qp+9qa%_77a
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
